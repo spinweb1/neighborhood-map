8989769a2836eb6af00662b710df58503466aba8
spinweb1 <spinweb1@yahoo.com>
2016-09-14 16:30:20 -0400

    added populateInfoWindow back, changed Lat, Lon to object literal

--- js/main.js
+++ js/main.js
@@ -3,72 +3,60 @@
 var locations = 
 	[
 	  {
-		name: 'North Carolina Museum of Science',
-		lat: 35.782274,
-		lng: -78.639550
+		title: 'North Carolina Museum of Science',
+		location: {lat: 35.782274,lng: -78.639550}
 	  },
 	  {
-		name: 'Raleigh Convention Center',
-		lat: 35.773646,
-		lng: -78.641350
+		title: 'Raleigh Convention Center',
+		location: {lat: 35.773646,lng: -78.641350}
 	  },
 	  {
-		name: 'Lincoln Theater',
-		lat: 35.7795897,
-		lng: -78.6381787
+		title: 'Lincoln Theater',
+		location: {lat: 35.7795897,lng: -78.6381787}
 	  },
 	  {
-		name: 'Duke Energy Center for the Performing Arts',
-		lat: 35.771467,
-		lng: -78.639555
+		title: 'Duke Energy Center for the Performing Arts',
+		location: {lat: 35.771467,lng: -78.639555}
 	  },
 	  {
-		name: 'North Carolina State Capitol',
-		lat: 35.780568,
-		lng: -78.639117
+		title: 'North Carolina State Capitol',
+		location: {lat: 35.780568,lng: -78.639117}
 	  },
 	  {
-		name: 'William Peace University',
-		lat: 35.789286,
-		lng: -78.637452
+		title: 'William Peace University',
+		location: {lat: 35.789286,lng: -78.637452}
 	  },
 	  {
-		name: 'Central Prison',
-		lat: 35.776388,
-		lng: -78.656255
+		title: 'Central Prison',
+		location: {lat: 35.776388,lng: -78.656255}
 	  },
 	  {
-		name: 'Saint Augustine\'s University',
-		lat: 35.784858,
-		lng: -78.621310
+		title: 'Saint Augustine\'s University',
+		location: {lat: 35.784858,lng: -78.621310}
 	  },
 	  {
-		name: 'D.H. Hill Library',
-		lat: 35.787512,
-		lng: -78.669600
+		title: 'D.H. Hill Library',
+		location: {lat: 35.787512,lng: -78.669600}
 	  },
 	  {
-		name: 'North Carolina Museum of History',
-		lat: 35.781785,
-		lng: -78.638556
+		title: 'North Carolina Museum of History',
+		location: {lat: 35.781785,lng: -78.638556}
 	  },
 	  {
-		name: 'Wake County Justice Center',
-		lat: 35.776576,
-		lng: -78.641139
+		title: 'Wake County Justice Center',
+		location: {lat: 35.776576,lng: -78.641139}
 	  },
 	  {
-		name: 'Sacred Heart Cathedral',
-		lat: 35.780675,
-		lng: -78.641971
+		title: 'Sacred Heart Cathedral',
+		location: {lat: 35.780675,lng: -78.641971}
 	  }
 	];
 
 // Feature constructor function
 var Feature = function(data) {
 	this.name = ko.observable(data.name);
-	this.lat = ko.observable(data.lat);
-	this.lng = ko.observable(data.lng);
+	this.location = ko.observable(data.location);
+	//this.lng = ko.observable(data.lng);
 };
 
 
